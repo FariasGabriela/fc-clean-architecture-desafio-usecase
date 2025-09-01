8dcc3c05b25443c213a2d9bd0385a8eeea100b14
FariasGabriela <gabrielafarias@alunos.utfpr.edu.br>
2025-09-01 07:22:40 -0300

    Adicionado teste de notification pattern

--- src/domain/@shared/notification/notification.spec.ts
+++ src/domain/@shared/notification/notification.spec.ts
@@ -1,6 +1,6 @@
 import Notification from "./notification";
 
-describe("Unit testss for notifications", () => {
+describe("Unit tests for notifications", () => {
   it("should create errors", () => {
     const notification = new Notification();
     const error = {
@@ -52,6 +52,64 @@ describe("Unit testss for notifications", () => {
     const error = {
       message: "error message",
       context: "customer",
+    };
+    notification.addError(error);
+
+    expect(notification.getErrors()).toEqual([error]);
+  });
+
+  //product
+    it("should create errors to product", () => {
+    const notification = new Notification();
+    const error = {
+      message: "error message",
+      context: "product",
+    };
+
+    notification.addError(error);
+
+    expect(notification.messages("product")).toBe("product: error message,");
+
+    const error2 = {
+      message: "error message2",
+      context: "product",
+    };
+    notification.addError(error2);
+
+    expect(notification.messages("product")).toBe(
+      "product: error message,product: error message2,"
+    );
+
+    const error3 = {
+      message: "error message3",
+      context: "order",
+    };
+    notification.addError(error3);
+
+    expect(notification.messages("product")).toBe(
+      "product: error message,product: error message2,"
+    );
+    expect(notification.messages()).toBe(
+      "product: error message,product: error message2,order: error message3,"
+    );
+  });
+
+  it("should check if notification of product has at least one error", () => {
+    const notification = new Notification();
+    const error = {
+      message: "error message",
+      context: "product",
+    };
+    notification.addError(error);
+
+    expect(notification.hasErrors()).toBe(true);
+  });
+
+    it("should get all errors from product props", () => {
+    const notification = new Notification();
+    const error = {
+      message: "error message",
+      context: "product",
     };
     notification.addError(error);
 
